b5088c1f9242d9e04b9cc0fdce1be131bca129d1
Welbert Castro <welberthime@gmail.com>
2022-05-25 17:12:12 -0300

    Use env_var instead of packages

--- dbt/models/trusted/covid19_open_data.sql
+++ dbt/models/trusted/covid19_open_data.sql
@@ -3,5 +3,5 @@
 SELECT *
 FROM {{ source('covid19_open_data', 'covid19_open_data') }}
 WHERE subregion1_code = 'IA'
-  AND country_code = 'USA'
-  AND date = '{{ dbt_airflow_macros.ds()}}'
+  AND country_code = 'US'
+  AND date = '{{ env_var("EXECUTION_DATE", "") }}'
--- dbt/models/trusted/iowa_liquor_sales.sql
+++ dbt/models/trusted/iowa_liquor_sales.sql
@@ -3,4 +3,4 @@
 SELECT *
 FROM {{ source('iowa_liquor', 'sales') }}
 WHERE county IS NOT NULL
-  AND date = '{{ dbt_airflow_macros.ds()}}'
+  AND date = '{{ env_var("EXECUTION_DATE", "") }}'
