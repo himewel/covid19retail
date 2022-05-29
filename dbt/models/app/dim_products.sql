e2693322e7cd7967abd62aa5352c40287ba6715e
Welbert Castro <welberthime@gmail.com>
2022-05-28 23:19:35 -0300

    Deduplicate values

--- dbt/models/app/dim_products.sql
+++ dbt/models/app/dim_products.sql
@@ -1,6 +1,22 @@
 {{ config(materialized='table') }}
 
-  SELECT DISTINCT
+  WITH products AS (
+    SELECT
+      ROW_NUMBER() OVER(PARTITION BY item_number ORDER BY item_description) AS rank_val,
+      item_number,
+      category,
+      vendor_number,
+      item_description,
+      pack,
+      bottle_volume_ml,
+      state_bottle_cost,
+      state_bottle_retail
+    FROM {{ ref('iowa_liquor_sales') }}
+    WHERE item_number IS NOT NULL
+      AND item_description IS NOT NULL
+  )
+
+  SELECT
     CAST(item_number AS INT64) AS CdProduct,
     CAST(CAST(category AS FLOAT64) AS INT64) AS CdCategory,
     CAST(CAST(vendor_number AS FLOAT64) AS INT64) AS CdVendor,
@@ -9,6 +25,5 @@
     bottle_volume_ml AS NuBottleVolumeMl,
     state_bottle_cost AS VrStateBottleCost,
     state_bottle_retail AS VrStateBottleRetail
-  FROM {{ ref('iowa_liquor_sales') }}
-  WHERE item_number IS NOT NULL
-    AND item_description IS NOT NULL
+  FROM products
+  WHERE rank_val = 1
--- dbt/models/app/dim_vendors.sql
+++ dbt/models/app/dim_vendors.sql
@@ -1,8 +1,19 @@
 {{ config(materialized='table') }}
 
-  SELECT DISTINCT
-    CAST(CAST(vendor_number AS FLOAT64) AS INT64) AS CdVendor,
+  WITH vendors AS (
+    SELECT
+      CAST(CAST(vendor_number AS FLOAT64) AS INT64) AS vendor_number,
+      vendor_name,
+      ROW_NUMBER() OVER(
+        PARTITION BY CAST(CAST(vendor_number AS FLOAT64) AS INT64)
+        ORDER BY date
+      ) AS rank_val
+    FROM {{ ref('iowa_liquor_sales') }}
+    WHERE vendor_number IS NOT NULL
+      AND vendor_name IS NOT NULL
+  )
+  SELECT
+    vendor_number AS CdVendor,
     vendor_name AS DsVendorName
-  FROM {{ ref('iowa_liquor_sales') }}
-  WHERE vendor_number IS NOT NULL
-    AND vendor_name IS NOT NULL
+  FROM vendors
+  WHERE rank_val = 1
