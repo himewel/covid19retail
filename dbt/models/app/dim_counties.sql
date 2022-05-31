ad2db20f5480ad6b91597e8b69ab051662a04de1
Welbert Castro <welberthime@gmail.com>
2022-05-30 22:26:10 -0300

    Update fact tables schema

--- dbt/models/app/dim_counties.sql
+++ dbt/models/app/dim_counties.sql
@@ -18,16 +18,11 @@
       population,
       population_male,
       population_female,
-      population_density,
-      human_development_index,
-      gdp_usd,
-      gdp_per_capita_usd,
       openstreetmap_id,
       latitude,
       longitude,
       ST_ASTEXT(location_geometry) AS location_geometry,
-      area_sq_km,
-      hospital_beds_per_1000
+      area_sq_km
     FROM {{ ref('covid19_open_data') }}
     WHERE aggregation_level = 2
       AND subregion1_code = 'IA'
@@ -45,16 +40,11 @@
     covid19_counties.population AS QtPopulation,
     covid19_counties.population_male AS QtPopulationMale,
     covid19_counties.population_female AS QtPopulationFemale,
-    covid19_counties.population_density AS QtPopulationDensity,
-    covid19_counties.human_development_index AS NuHumanIndexDevelopment,
-    covid19_counties.gdp_usd AS NuGdpUsd,
-    covid19_counties.gdp_per_capita_usd AS NuGdpUsdPerCapita,
     covid19_counties.openstreetmap_id AS CdOpenStreetMap,
     covid19_counties.latitude AS NuLatitude,
     covid19_counties.longitude AS NuLongitude,
     ST_GEOGFROMTEXT(covid19_counties.location_geometry) AS CdLocationGeometry,
-    covid19_counties.area_sq_km AS NuAreqSqKm,
-    covid19_counties.hospital_beds_per_1000 AS QtHospitalBedsPer1000
+    covid19_counties.area_sq_km AS NuAreqSqKm
   FROM covid19_counties
   LEFT JOIN iowa_counties
     ON SUBSTR(LOWER(REGEXP_REPLACE(covid19_counties.subregion2_name, ' County|\'', '')), 0, 10) = iowa_counties.county
--- dbt/models/app/fact_events.sql
+++ dbt/models/app/fact_events.sql
@@ -1,8 +1,7 @@
 {{ config(materialized='incremental', unique_key='CdEvent') }}
 
   SELECT
-    SHA1(CONCAT(counties.CdCounty, covid19.date)) AS CdEvent,
-    counties.CdCounty,
+    SHA1(CAST(covid19.date AS STRING)) AS CdEvent,
     dates.CdDate,
     covid19.school_closing AS InSchoolClosing,
     covid19.workplace_closing AS InWorkspaceClosing,
@@ -14,21 +13,28 @@
   	covid19.international_travel_controls AS InInternationalTravelControls,
   	covid19.income_support AS InIncomeSupport,
   	covid19.debt_relief AS InDebtRelief,
-  	covid19.fiscal_measures AS InFiscalMeasures,
-  	covid19.international_support AS InInternationalSupport,
   	covid19.public_information_campaigns AS InPublicInformationCampaigns,
   	covid19.testing_policy AS InTestingPolicy,
   	covid19.contact_tracing AS InContactTracing,
-  	covid19.emergency_investment_in_healthcare AS InEmergencyInvestmentInHealthcare,
   	covid19.investment_in_vaccines AS InInvestmentInVaccines,
   	covid19.facial_coverings AS InFacialCoverings,
   	covid19.vaccination_policy AS InVaccinationPolicy,
   	covid19.stringency_index AS InStringencyIndex,
-  	covid19.average_temperature_celsius AS NuAveTempCelsius,
-  	covid19.minimum_temperature_celsius AS NuMinTempCelsius,
-    covid19.maximum_temperature_celsius AS NuMaxTempCelsius
+    covid19.new_persons_vaccinated AS QtPersonsVaccinated,
+    covid19.new_vaccine_doses_administered AS QtVaccineDosesAdministered,
+  	covid19.new_tested AS QtTested,
+  	covid19.new_recovered AS QtRecovered,
+  	covid19.new_hospitalized_patients AS QtHospitalizedPatients,
+    covid19.current_hospitalized_patients AS QtCurrentHospitalizedPatients,
+    covid19.current_intensive_care_patients AS QtCurrentIntensiveCarePatients,
+    covid19.current_ventilator_patients AS QtCurrentVentilatorPatient,
+  	covid19.new_persons_fully_vaccinated_pfizer AS QtPersonsFullyVaccinatedPfizer,
+  	covid19.new_vaccine_doses_administered_pfizer AS QtVaccineDosesAdministeredPfizer,
+  	covid19.new_persons_fully_vaccinated_moderna AS QtPersonsFullyVaccinatedModerna,
+  	covid19.new_vaccine_doses_administered_moderna AS QtVaccineDosesAdministeredModerna,
+  	covid19.new_persons_fully_vaccinated_janssen AS QtPersonsFullyVaccinatedJanssen,
+  	covid19.new_vaccine_doses_administered_janssen AS QtVaccineDosesAdministeredJanssen
   FROM {{ ref('covid19_open_data') }} AS covid19
-  JOIN {{ ref('dim_counties') }} AS counties
-    ON covid19.subregion2_name = counties.DsCountyName
   JOIN {{ ref('dim_dates') }} AS dates
     ON covid19.date = dates.DsDate
+  WHERE covid19.aggregation_level = 1
